9b707734c0a741997bc883ca6d418eec14f3de15
RummahH <jah.hasant@gmail.com>
2023-09-30 12:52:03 +0600

    initials

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5001; // You can choose any available port
+
+// Define a route for the "/hello" endpoint
+app.get("/hello", (req, res) => {
+  res.send("Hello");
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
