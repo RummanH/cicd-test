2d4315c14f087d2a324d2b55a953625c047292b0
RummahH <jah.hasant@gmail.com>
2023-09-30 13:39:05 +0600

    check cicd

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5001; // You can choose any available port
 
-// Define a route for the "/hello" endpoint
+// Define a route for the "/hello" endpoints
 app.get("/hello", (req, res) => {
   res.send("Hello");
 });
